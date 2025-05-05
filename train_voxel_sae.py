908c3f82a3b9dafe7494188399e3f0df9fe13dce
cest-andre <ajl321@hotmail.com>
2025-05-05 04:17:15 +0000

    Voxel subset now taken with RNG

--- train_voxel_sae.py
+++ train_voxel_sae.py
@@ -14,29 +14,34 @@ if __name__ == "__main__":
     parser.add_argument('--ckpt_dir', type=str)
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--start_epoch', type=int, default=0)
     parser.add_argument('--expansion', type=int, default=32)
     parser.add_argument('--topk', type=int, default=32)
-    parser.add_argument('--num_voxels', type=int, default=5931)
+    parser.add_argument('--num_voxels', type=int, default=0)
     parser.add_argument('--device', type=int)
     args = parser.parse_args()
+    assert args.start_epoch < args.epochs
 
     torch.manual_seed(0)
     device = f"cuda:{args.device}" if torch.cuda.is_available() else "cpu"
 
     voxel_data = np.load(args.voxel_path)
     voxel_data = torch.tensor(voxel_data)
-    voxel_ds = TensorDataset(voxel_data)
-    dataloader = DataLoader(voxel_ds, batch_size=args.batch_size, shuffle=True)
-    input_dims = voxel_data[0].shape[-1]
+    if args.num_voxels > 0 and args.num_voxels < voxel_data.shape[1]:
+        voxel_data = voxel_data[:, torch.randperm(voxel_data.shape[1])[:args.num_voxels]]
 
-    sae = SAE(args.num_voxels, args.expansion, topk=args.topk if args.topk > 0 else None, auxk=128, dead_steps_threshold=16, device='cpu', enc_data=None).to(device)
-    # sae.load_state_dict(torch.load(f"{args.ckpt_dir}/{label}_sae_weights_{10}ep.pth"))
+    voxel_ds = TensorDataset(voxel_data)
+    dataloader = DataLoader(voxel_ds, batch_size=args.batch_size, shuffle=True, drop_last=True)
 
     lr=0.0004
     adam_beta1=0.9
     adam_beta2=0.999
     aux_alpha = 256
-    label = f'top{args.topk}_{args.num_voxels}subset_{args.batch_size}batch_{aux_alpha}aux_{args.expansion}exp' if args.topk > 0 else f'vanilla_{args.expansion}exp'
+    label = f'top{args.topk}_{args.num_voxels}subset_rand_{aux_alpha}aux_{args.expansion}exp' if args.topk > 0 else f'vanilla_{args.expansion}exp'
+
+    sae = SAE(args.num_voxels, args.expansion, topk=args.topk if args.topk > 0 else None, auxk=512, dead_steps_threshold=64, device='cpu', enc_data=None).to(device)
+    if args.start_epoch > 0:
+        sae.load_state_dict(torch.load(f"{args.ckpt_dir}/{label}_sae_weights_{args.start_epoch}ep.pth"))
 
     mse_scale = (
         1 / ((voxel_data.float().mean(dim=0) - voxel_data.float()) ** 2).mean()
@@ -54,7 +59,7 @@ if __name__ == "__main__":
 
     all_mses = []
     mse = torch.nn.functional.mse_loss
-    for ep in range(0, args.epochs):
+    for ep in range(args.start_epoch, args.epochs):
         print(f"Epoch {ep}")
         total_mse = 0
         total_l1 = 0
@@ -62,7 +67,7 @@ if __name__ == "__main__":
         for i, voxels in enumerate(dataloader, 0):
             optimizer.zero_grad()
 
-            voxels = voxels[0][:, :args.num_voxels].to(device)
+            voxels = voxels[0].to(device)
             latents, voxels_hat, dead_voxels_recon, num_dead = sae(voxels)
 
             mse_loss = mse_scale * mse(voxels_hat, voxels, reduction="none").sum(-1)
--- viz_voxels.py
+++ viz_voxels.py
@@ -8,7 +8,7 @@ from torch.utils.data import TensorDataset, DataLoader
 from data_utils import extract_shared_for_subj, save_top_cocos
 
 
-#   TODO: Create dataloader from val_voxels and compute latent acts via dot product of voxel responses and latent's decoder weights.
+#   TODO: Create dataloader from voxel_data and compute latent acts via dot product of voxel responses and latent's decoder weights.
 #         Record acts to all latents for all stimuli, sort by descending act, use indices from this sort to index coco_ids (follow imnet_acts logic).
 #         Pass to a top-K image saver function which takes in indices and coco_ids list and loads the top-K images, then saves to savedir.
 #         Allow option to perform on raw voxels, or define separate function is easier.
@@ -24,15 +24,19 @@ if __name__ == "__main__":
     parser.add_argument('--savedir', type=str)
     parser.add_argument('--subj_id', type=int)
     parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--num_voxels', type=int, default=5931)
+    parser.add_argument('--num_voxels', type=int, default=0)
     parser.add_argument('--device', type=int)
     args = parser.parse_args()
 
     device = f"cuda:{args.device}" if torch.cuda.is_available() else "cpu"
+    torch.manual_seed(0)
 
-    coco_ids, val_voxels = extract_shared_for_subj(args.splits_path, args.nsd_path, args.subj_id)
-    val_voxels = torch.tensor(val_voxels)
-    voxel_ds = TensorDataset(val_voxels)
+    coco_ids, voxel_data = extract_shared_for_subj(args.splits_path, args.nsd_path, args.subj_id)
+    voxel_data = torch.tensor(voxel_data)
+    if args.num_voxels > 0 and args.num_voxels < voxel_data.shape[1]:
+        voxel_data = voxel_data[:, torch.randperm(voxel_data.shape[1])[:args.num_voxels]]
+
+    voxel_ds = TensorDataset(voxel_data)
     dataloader = DataLoader(voxel_ds, batch_size=args.batch_size, shuffle=False, drop_last=False)
 
     latent_weights = torch.load(os.path.join(args.weights_dir, f'subj{args.subj_id}', f'{args.sae_name}.pth'))['W_dec'].to(device).T
@@ -40,7 +44,7 @@ if __name__ == "__main__":
     im_count = 0
     all_latent_acts = []
     for i, voxels in enumerate(dataloader):
-        voxels = voxels[0][:, :args.num_voxels].to(device)
+        voxels = voxels[0].to(device)
         im_count += voxels.shape[0]
 
         latent_acts = voxels @ latent_weights
